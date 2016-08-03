d5331ec6dfa6f9f6cda83295a931b387391edc81
Andrew Chalkley <andrew@chalkley.org>
2016-08-03 12:49:12 -0700

    Added tweets

--- src/Episode.js
+++ src/Episode.js
@@ -22,6 +22,10 @@ class Episode extends Component {
                 <a href="https://www.youtube.com/user/thatmakershow">YouTube Channel</a> - <a href="https://itunes.apple.com/us/podcast/that-maker-show/id843687042">iTunes Podcast</a>
                 <h4>Hosted by</h4>
                 <ul>{Object.keys(this.props.hosts).map(this.renderHost.bind(this))}</ul>
+                <h4>Producers by</h4>
+                <ul>{Object.keys(this.props.producers).map(this.renderProducers.bind(this))}</ul>
+                <h4>Special Thanks</h4>
+                <ul>{Object.keys(this.props.special_thanks).map(this.renderSpecialThanks.bind(this))}</ul>
                 <h4>Behind the Scences</h4>
                 <p>Here's some photos from behind the scenes of the <em>{this.props.title}</em> episode</p>
                 <Gallery photos={this.props.gallery} />
@@ -33,6 +37,20 @@ class Episode extends Component {
         const link = this.props.hosts[host];
         return (
             <li key={host}><a href={link}>{host}</a></li>
+        );
+    }
+
+    renderProducers(producer) {
+        const link = this.props.producers[producer];
+        return (
+            <li key={producer}><a href={link}>{producer}</a></li>
+        );
+    }
+
+    renderSpecialThanks(thanks) {
+        const link = this.props.special_thanks[thanks];
+        return (
+            <li key={thanks}><a href={link}>{thanks}</a></li>
         );
     }
 
@@ -56,6 +74,7 @@ class Episode extends Component {
     }
 
     _getCurrentTil(seconds) {
+        console.log(seconds);
         const currentTils = this.props.tils.filter(til => til.time <= seconds).pop();
         if(currentTils) {
             this.setState({message: this._createMessageWithTime(currentTils.message, currentTils.time)});
